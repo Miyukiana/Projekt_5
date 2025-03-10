2fc7598870c5260239e7d6fd36c19f0786f5c331
Miyukiana <weber.selina@icloud.com>
2025-03-10 17:40:10 +0100

    animal probably works

--- script.js
+++ script.js
@@ -43,15 +43,13 @@ function getAnimal(){
         document.getElementById("output3").innerText = "Please enter an animal.";
         return;
     }
-    if ([
-        "dog", "cat", "fish", "bird", "rabbit", "horse", "butterfly", "shark", "giraffe", "elephant", "bee",
-        "lion", "tiger", "bear", "monkey", "panda", "penguin", "kangaroo", "koala", "crocodile", "snake",
-        "turtle", "owl", "fox", "wolf", "deer", "zebra", "rhinoceros", "hippopotamus", "camel", "flamingo",
-        "parrot", "dolphin", "octopus", "jellyfish", "starfish", "seahorse", "crab", "lobster", "snail",
-        "ant", "spider", "scorpion", "mosquito", "fly", "ladybug", "beetle", "dragonfly"
-      ].includes(animal)) {
-        console.log("Animal is in the list!");
-      document.getElementById("output3").innerText = animal + "is a beautiful animal!";
+    if (
+        "dog"|| "cat"|| "fish"|| "bird"|| "rabbit"|| "horse"|| "butterfly"|| "shark"|| "giraffe"|| "elephant"|| "bee"||
+"lion"|| "tiger"|| "bear"|| "monkey"|| "panda"|| "penguin"|| "kangaroo"|| "koala"|| "crocodile"|| "snake"||
+"turtle"|| "owl"|| "fox"|| "wolf"|| "deer"|| "zebra"|| "rhinoceros"|| "hippopotamus"|| "camel"|| "flamingo"||
+"parrot"|| "dolphin"|| "octopus"|| "jellyfish"|| "starfish"|| "seahorse"|| "crab"|| "lobster"|| "snail"||
+"ant"|| "spider"|| "scorpion"|| "mosquito"|| "fly"|| "ladybug"|| "beetle"|| "dragonfly" ) 
+{ document.getElementById("output3").innerText = animal + "is a beautiful animal!";
         return;
     }
 }
